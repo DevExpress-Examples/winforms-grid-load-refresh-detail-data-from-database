fcd7dd46b5daf9f9fa05ee91167c70607e89412d
SvetlanaPN <svetlana.peksheva@devexpress.com>
2020-06-02 20:20:52 +0300

    dispose connection

--- CS/WindowsApplication297/Form1.cs
+++ CS/WindowsApplication297/Form1.cs
@@ -11,12 +11,9 @@ using DevExpress.XtraGrid.Views.Base;
 using DevExpress.XtraGrid.Views.Grid;
 using DevExpress.XtraGrid.Columns;
 
-namespace WindowsApplication297
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace WindowsApplication297 {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
 
             DataTable dt = new DataTable();
@@ -24,86 +21,67 @@ namespace WindowsApplication297
             gridControl1.DataSource = dt.DefaultView;
         }
 
-        private object GetRowKey(ColumnView view, int rowHandle)
-        {
+        private object GetRowKey(ColumnView view, int rowHandle) {
             return view.GetRowCellValue(rowHandle, "CustomerID");
         }
 
-        string GetDetailQuery(object key)
-        {
+        string GetDetailQuery(object key) {
             return string.Format("SELECT * FROM ORDERS WHERE CustomerID='{0}'", key);
         }
 
-        Hashtable cache = new Hashtable();
+        Dictionary<object, IList> cache = new Dictionary<object, IList>();
 
-        OleDbConnection connection;
-
-        private int LoadData(DataTable table, string query)
-        {
-            if (connection == null)
-            {
-                connection = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\nwind.mdb;Persist Security Info=True");
-            }
-            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
-            try
-            {
-                int num = adapter.Fill(table);
-                textBox1.AppendText(string.Format("{0} data rows fetched" + Environment.NewLine, num));
-                return num;
-            }
-            catch { return 0; }
-            finally
-            {
-                adapter.Dispose();
+        private int LoadData(DataTable table, string query) {
+            using(var connection = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\nwind.mdb;Persist Security Info=True")) {
+                OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+                try {
+                    int num = adapter.Fill(table);
+                    textBox1.AppendText(string.Format("{0} data rows fetched" + Environment.NewLine, num));
+                    return num;
+                }
+                catch { return 0; }
+                finally {
+                    adapter.Dispose();
+                }
             }
         }
 
-        private IList GetDetailData(ColumnView view, int rowHandle)
-        {
+        private IList GetDetailData(ColumnView view, int rowHandle) {
             object key = GetRowKey(view, rowHandle);
-            IList list = cache[key] as IList;
-            if (list == null)
-            {
+            if(!cache.ContainsKey(key)) {
                 DataTable dt = new DataTable();
                 LoadData(dt, GetDetailQuery(key));
-                list = dt.DefaultView;
-                cache[key] = list;
+                cache[key] = dt.DefaultView;
             }
-            return list;
+            return cache[key];
         }
 
-        private void simpleButton1_Click(object sender, EventArgs e)
-        {
+        private void simpleButton1_Click(object sender, EventArgs e) {
             ColumnView view = (ColumnView)gridControl1.FocusedView;
-            if (!view.IsDetailView) return;
+            if(!view.IsDetailView) return;
 
             object key = GetRowKey((ColumnView)view.ParentView, view.SourceRowHandle);
             DataView dv = cache[key] as DataView;
-            if (dv != null)
-            {
+            if(dv != null) {
                 DataTable dt = dv.Table;
                 dt.Clear();
                 LoadData(dt, GetDetailQuery(key));
             }
         }
 
-        private void gridView1_MasterRowEmpty(object sender, DevExpress.XtraGrid.Views.Grid.MasterRowEmptyEventArgs e)
-        {
+        private void gridView1_MasterRowEmpty(object sender, DevExpress.XtraGrid.Views.Grid.MasterRowEmptyEventArgs e) {
             e.IsEmpty = false;
         }
 
-        private void gridView1_MasterRowGetChildList(object sender, DevExpress.XtraGrid.Views.Grid.MasterRowGetChildListEventArgs e)
-        {
+        private void gridView1_MasterRowGetChildList(object sender, DevExpress.XtraGrid.Views.Grid.MasterRowGetChildListEventArgs e) {
             e.ChildList = GetDetailData((ColumnView)sender, e.RowHandle);
         }
 
-        private void gridView1_MasterRowGetRelationCount(object sender, DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationCountEventArgs e)
-        {
+        private void gridView1_MasterRowGetRelationCount(object sender, DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationCountEventArgs e) {
             e.RelationCount = 1;
         }
 
-        private void gridView1_MasterRowGetRelationName(object sender, DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationNameEventArgs e)
-        {
+        private void gridView1_MasterRowGetRelationName(object sender, DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationNameEventArgs e) {
             e.RelationName = "Details";
         }
     }
